580941057bb850280eee7d6522be0a15ce2f5c09
Chinh Do <do.inby@gmail.com>
2021-08-18 17:13:41 +1000

    added function to loop and display image on click

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i < 6; i++) {
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", `images/pic${i}.jpg`);
+    thumbBar.appendChild(newImage);
+    
+    // Adding mouse click behavior to each thumbnail
+    newImage.addEventListener("click", function () {
+        displayedImage.setAttribute("src", `images/pic${i}.jpg`);
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
